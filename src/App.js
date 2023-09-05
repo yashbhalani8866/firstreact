418d336f1b3fd7958cc79042beca8b57b3de93ad
yashbhalani8866 <yashbhalani8866@gmail.com>
2023-09-05 16:54:54 +0530

    change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React change
         </a>
       </header>
     </div>
