5231f773e539f2976f1cd12d23a58ad69b4926c7
yashbhalani8866 <yashbhalani8866@gmail.com>
2023-09-15 17:45:52 +0530

    category

---
+++ src/containers/NewProduct.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+
+function NewProduct(props) {
+
+    const [product, setProduct] = useState([]);
+    const [search,setSearch] = useState('');
+    const [filterData,setFilterData] = useState('');
+
+    const getData = async () => {
+        const response = await fetch("https://dummyjson.com/products")
+
+        const data = await response.json();
+        console.log(data);
+
+        setProduct(data.products);
+
+    }
+
+    useEffect(() => {
+        getData();
+    }, [])
+
+    const handleSearch = (value) => {
+        setSearch(value);
+        let Fdata = product.filter((v) => 
+            v.category.toLowerCase().includes(value.toLowerCase())
+            )
+            console.log(Fdata);
+        setFilterData(Fdata);
+
+    }
+
+    const FinalData = filterData.length > 0 ? filterData : product ;
+    console.log(FinalData);
+
+
+    return (
+        <>
+            <button onClick={() => handleSearch('smartphones')}>smartphones</button>
+            <button onClick={() => handleSearch('laptops')}>laptops</button>
+            <button onClick={() => handleSearch('fragrances')}>fragrances</button>
+            <button onClick={() => handleSearch('skincare')}>skincare</button>
+            <button onClick={() => handleSearch('groceries')}>groceries</button>
+            <button onClick={() => handleSearch('home-decoration')}>home-decoration</button>
+            <button onClick={() => handleSearch('')}>All</button>
+            <div className='row'>
+                {FinalData.map((v) => {
+                    return (
+                        <div className='col-md-4' style={{  border: '1px solid black', padding: '50px'}}>
+                            <h3>{v.brand}</h3>
+                            <img src={v.images[0]} style={{width: '200px',height:'150px'}}/> 
+                            <h4>$ {v.price}</h4>
+                            <p>{v.description}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        </>
+    );
+}
+
+export default NewProduct;
